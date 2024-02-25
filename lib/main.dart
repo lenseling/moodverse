dddf161d54a81b7b359c7c21ce8e6964d4a3ca01
Lili Enseling <liliyang@ad.unc.edu>
2024-02-25 01:55:08 -0500

    Added google font

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:auth0_flutter/auth0_flutter.dart';
 
 const appScheme = 'flutterdemo';
 
@@ -21,6 +21,9 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Color(0x5302E3)),
+          textTheme: GoogleFonts.latoTextTheme(
+            Theme.of(context).textTheme,
+          ),
         ),
         home: MyHomePage(),
       ),
@@ -266,68 +269,6 @@ class LandingPage extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class Profile extends StatelessWidget {
-  final Future<void> Function() logoutAction;
-  final UserProfile? user;
-
-  const Profile(this.logoutAction, this.user, {final Key? key})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          width: 150,
-          height: 150,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.blue, width: 4),
-            shape: BoxShape.circle,
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(user?.pictureUrl.toString() ?? ''),
-            ),
-          ),
-        ),
-        const SizedBox(height: 24),
-        Text('Name: ${user?.name}'),
-        const SizedBox(height: 48),
-        ElevatedButton(
-          onPressed: () async {
-            await logoutAction();
-          },
-          child: const Text('Logout'),
-        ),
-      ],
-    );
-  }
-}
-
-class Login extends StatelessWidget {
-  final Future<void> Function() loginAction;
-  final String loginError;
-
-  const Login(this.loginAction, this.loginError, {final Key? key})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        ElevatedButton(
-          onPressed: () async {
-            await loginAction();
-          },
-          child: const Text('Login'),
-        ),
-        Text(loginError ?? ''),
-      ],
     );
   }
 }
